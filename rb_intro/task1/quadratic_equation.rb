5473ca739d20ee6b36f6173bee7c69a12429bf35
birzhans <49227586+birzhans@users.noreply.github.com>
2020-08-25 16:40:07 +0600

    Update quadratic_equation.rb

--- rb_intro/task1/quadratic_equation.rb
+++ rb_intro/task1/quadratic_equation.rb
@@ -12,12 +12,12 @@ d = b**2 - (4 * a * c)
 
 if d > 0
   sqrt_d = Math.sqrt(d)
-	x1 = (-b - sqrt_d) / (2.0 * a)
-	x2 = (-b + sqrt_d) / (2.0 * a)
-	puts "Roots: #{x1}, #{x2}."
+  x1 = (-b - sqrt_d) / (2.0 * a)
+  x2 = (-b + sqrt_d) / (2.0 * a)
+  puts "Roots: #{x1}, #{x2}."
 elsif d == 0
-	x = -b / (2.0 * a)
-	puts "Root: #{x}."
+  x = -b / (2.0 * a)
+  puts "Root: #{x}."
 else
-	puts "No roots."
+  puts "No roots."
 end
