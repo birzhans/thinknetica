a8ee1321028b1216b8500cf4a36be11275ecc923
birzhans <49227586+birzhans@users.noreply.github.com>
2020-08-25 16:01:37 +0600

    Update is_right_triangle.rb

--- rb_intro/task1/is_right_triangle.rb
+++ rb_intro/task1/is_right_triangle.rb
@@ -12,18 +12,18 @@ hypotenuse = [a, b, c].max
 right_side = nil
 
 if hypotenuse == a
-	right_side = b ** 2 + c ** 2
+  right_side = b ** 2 + c ** 2
 elsif hypotenuse == b
-	right_side = a ** 2 + c ** 2
+  right_side = a ** 2 + c ** 2
 elsif hypotenuse == c
-	right_side = a ** 2 + b ** 2
+  right_side = a ** 2 + b ** 2
 end
 	
 
 if a == b && a == c
-	puts "This is an equilateral and isosceles triangle."
+  puts "This is an equilateral and isosceles triangle."
 elsif a == b || b == c
-	puts "This is an isosceles triangle."
+  puts "This is an isosceles triangle."
 elsif hypotenuse && (hypotenuse ** 2 == right_side)
-	puts "This is a right triangle"
+  puts "This is a right triangle"
 end
