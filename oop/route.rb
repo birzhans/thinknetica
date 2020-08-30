677b8cf98d44a791e1e91c0a2f6223d6a754a780
birzhans <49227586+birzhans@users.noreply.github.com>
2020-08-30 22:04:29 +0600

    Update route.rb

--- oop/route.rb
+++ oop/route.rb
@@ -1,9 +1,9 @@
 class Route
-  attr_reader :stations
+  attr_reader :stations, :start_station, :end_station
 
   def initialize(start_station, end_station)
-    @start = start_station
-    @end = end_station
+    @start_station = start_station
+    @end_station = end_station
     @stations = [start_station, end_station]
   end
 
