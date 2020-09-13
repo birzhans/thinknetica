f3c5b6f8a34ef1a10783355f9e635189639ae87c
birzhans <49227586+birzhans@users.noreply.github.com>
2020-09-13 19:12:22 +0600

    Update wagon.rb

--- part8/wagon.rb
+++ part8/wagon.rb
@@ -1,13 +1,29 @@
 require_relative 'validation'
 
 class Wagon
+  attr_reader :available_volume, :volume
+
   include Brand
   include Validation
 
-  def initialize(number, num_of_seats)
+  def initialize(number, volume)
     @name = number
+    @volume = volume
+    @available_volume = volume
     validate!
   end
+
+  def load(weight)
+    self.available_volume -= weight
+  end
+
+  def used_volume
+    self.volume - self.available_volume
+  end
+
+  private
+
+  attr_writer :available_volume
 end
 
 
