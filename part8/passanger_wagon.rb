764fb7b3a615ae2e6dd1ff768833898a5e754c37
birzhans <49227586+birzhans@users.noreply.github.com>
2020-09-13 19:11:59 +0600

    Update passanger_wagon.rb

--- part8/passanger_wagon.rb
+++ part8/passanger_wagon.rb
@@ -1,27 +1,9 @@
 require_relative 'wagon'
 
 class PassangerWagon < Wagon
-  attr_reader :available_seats, :num_of_seats
-  def initialize(number, num_of_seats)
-    super
-    @num_of_seats = num_of_seats
-    @available_seats = num_of_seats
+  def load
+    self.available_volume -= 1
   end
-
-  def take_seat
-    if self.available_seats > 0
-      self.available_seats -= 1
-    end
-  end
-
-  def occupied_seats
-    self.num_of_seats - self.available_seats
-  end
-
-  private
-
-  attr_writer :available_seats
-
 end
 
 
